ac5a9ad85028a7f4362f8c147a6b728dacd69d3a
Zorn Wang <zornw@foxmail.com>
2022-03-12 14:28:47 +0800

    eslint-init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        commonjs: true
+    },
+    extends: [
+        'eslint:recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+    }
+}
